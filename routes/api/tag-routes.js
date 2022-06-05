5444f09c2b95ff51fd1a5e99f851183222c1999c
Phil Pepin <ppepin34@gmail.com>
2022-06-05 18:22:38 -0400

    fix Tag.create route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -51,7 +51,7 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   // create a new tag
   Tag.create({
-    tag_name: req.tag_name,
+    tag_name: req.body.tag_name,
   })
     .then(dbTagData => res.json(dbTagData))
     .catch(err => {
