8a8661c2c0bcbb962937ef35267fcacc3b0e24d8
Phil Pepin <ppepin34@gmail.com>
2022-06-04 20:17:30 -0400

    most routes done

--- models/Product.js
+++ models/Product.js
@@ -20,7 +20,7 @@ Product.init(
       allowNull: false
     },
     price: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.DECIMAL(10,2),
       allowNull: false,
       validate: {
         isDecimal: true
@@ -29,6 +29,7 @@ Product.init(
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue: 10,
       validate: {
         isNumeric: true
       }
